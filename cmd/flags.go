67182c2f399f8446f48e0a46ed25cc3c0b8160c4
Piotrek Janus <pjanus@cloudentity.com>
2024-01-29 12:29:39 +0100

    fix

--- cmd/flags.go
+++ cmd/flags.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 func mustMarkRequired(cmd *cobra.Command, flags ...string) {
 	for _, flag := range flags {
-		err := cmd.MarkFlagRequired(flag)
+		err := cmd.MarkPersistentFlagRequired(flag)
 
 		if err != nil {
 			panic(err)
