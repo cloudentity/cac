81dfd16adc4d5b3b5dadc00d155be97d887bacff
Piotrek Janus <pjanus@cloudentity.com>
2024-02-09 14:41:58 +0100

    escape output

--- scripts/action-entrypoint.sh
+++ scripts/action-entrypoint.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 /app/cac "$@" > /tmp/out
 
 # Merge the output into a single line so it can be used as github action output
 content=$(awk '{printf "%s\\n", $0}' /tmp/out)
-echo "::set-output name=result::$content"
+escaped=$(printf '%q' "$content")
+echo "::set-output name=result::$escaped"
