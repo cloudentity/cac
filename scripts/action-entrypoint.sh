11bb867b425830dd6d3ed690f2bc30e9958e0b56
Piotrek Janus <pjanus@cloudentity.com>
2024-02-13 14:42:16 +0100

    fix cac path

--- scripts/action-entrypoint.sh
+++ scripts/action-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cac "$@" > /tmp/out
+/app/cac "$@" > /tmp/out
 
 # Merge the output into a single line so it can be used as github action output
 content=$(awk '{printf "%s\\n", $0}' /tmp/out)
