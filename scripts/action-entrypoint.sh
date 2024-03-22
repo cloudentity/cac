f48ae25baa5398191850ec14ce1672ac10d67b78
Piotr Janus <pjanus@secureauth.com>
2024-03-22 01:07:16 +0100

    rollback some old changes

--- scripts/action-entrypoint.sh
+++ scripts/action-entrypoint.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 /app/cac "$@" > /tmp/out
 
 # Merge the output into a single line so it can be used as github action output
 content=$(awk '{printf "%s\\n", $0}' /tmp/out)
-escaped=$(printf '%q' "$content")
-echo "::set-output name=result::$escaped"
+echo "::set-output name=result::$content"
