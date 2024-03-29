9da670dae3f9ee31a609b047ec2bb59aca8cf967
Piotr Janus <pjanus@secureauth.com>
2024-03-29 10:39:22 +0100

    remove unused fun

--- internal/cac/data/validator.go
+++ internal/cac/data/validator.go
@@ -5,11 +5,3 @@ import "github.com/cloudentity/acp-client-go/clients/hub/models"
 type ValidatorApi interface {
 	Validate(data *models.Rfc7396PatchOperation) error
 }
-
-func CreateValidator(tenant bool) ValidatorApi {
-	if tenant {
-		return &TenantValidator{}
-	}
-
-	return &ServerValidator{}
-}
