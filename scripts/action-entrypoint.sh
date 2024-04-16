8323d57fb5300266984708e936c1f4330ca27c63
Piotr Janus <pjanus@secureauth.com>
2024-04-16 12:10:05 +0200

    use -e flag to fail entrypoint script

--- scripts/action-entrypoint.sh
+++ scripts/action-entrypoint.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
+set -e
 
 /app/cac "$@" > /tmp/out
-
-if [ $? -ne 0 ]; then
-    # save status to variable
-    status=$?
-fi
 
 # Merge the output into a single line so it can be used as github action output
 content=$(awk '{printf "%s\\n", $0}' /tmp/out)
 echo "::set-output name=result::$content"
-
-if [ $status -ne 0 ]; then
-    exit $status
-fi
