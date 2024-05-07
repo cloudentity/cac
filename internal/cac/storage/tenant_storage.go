d3685ca5b77b8b3c8486e19b6a291acbf2f5817c
Janus <pjanus@cloudentity.com>
2024-05-07 10:44:10 +0100

    Improved support for themes and templates (#18)
    * templates
    
    * fix reading theme
    
    * fix tests
    
    * themes test
    
    ---------
    
    Co-authored-by: Piotr Janus <pjanus@secureauth.com>

--- internal/cac/storage/tenant_storage.go
+++ internal/cac/storage/tenant_storage.go
@@ -1,138 +1,222 @@
 package storage
 
 import (
-	"context"
-	"github.com/cloudentity/acp-client-go/clients/hub/models"
-	"github.com/cloudentity/cac/internal/cac/api"
-	"github.com/cloudentity/cac/internal/cac/utils"
-	"path/filepath"
+    "context"
+    "github.com/cloudentity/acp-client-go/clients/hub/models"
+    "github.com/cloudentity/cac/internal/cac/api"
+    "github.com/cloudentity/cac/internal/cac/utils"
+    "path/filepath"
 )
 
 func InitTenantStorage(config *Configuration) Storage {
-	return &TenantStorage{
-		Config:        config,
-		ServerStorage: InitServerStorage(config),
-	}
+    return &TenantStorage{
+        Config:        config,
+        ServerStorage: InitServerStorage(config),
+    }
 }
 
 type TenantStorage struct {
-	Config        *Configuration
-	ServerStorage Storage
+    Config        *Configuration
+    ServerStorage Storage
 }
 
 func (t *TenantStorage) Write(ctx context.Context, data models.Rfc7396PatchOperation, opts ...api.SourceOpt) error {
-	var (
-		path  = t.Config.DirPath
-		model *models.TreeTenant
-		err   error
-	)
+    var (
+        path  = t.Config.DirPath
+        model *models.TreeTenant
+        err   error
+    )
 
-	if model, err = utils.FromPatchToModel[models.TreeTenant](data); err != nil {
-		return err
-	}
+    if model, err = utils.FromPatchToModel[models.TreeTenant](data); err != nil {
+        return err
+    }
 
-	if err = writeFiles(model.Pools,
-		filepath.Join(path, "pools"),
-		func(id string, it models.TreePool) string { return it.Name }); err != nil {
-		return err
-	}
+    if err = writeFiles(model.Pools,
+        filepath.Join(path, "pools"),
+        func(id string, it models.TreePool) string { return it.Name }); err != nil {
+        return err
+    }
 
-	if err = writeFiles(model.Schemas,
-		filepath.Join(path, "schemas"),
-		func(id string, it models.TreeSchema) string { return it.Name }); err != nil {
-		return err
-	}
+    if err = writeFiles(model.Schemas,
+        filepath.Join(path, "schemas"),
+        func(id string, it models.TreeSchema) string { return it.Name }); err != nil {
+        return err
+    }
 
-	if err = writeFiles(model.MfaMethods,
-		filepath.Join(path, "mfa_methods"),
-		func(id string, it models.TreeMFAMethod) string { return it.Mechanism }); err != nil {
-		return err
-	}
+    if err = writeFiles(model.MfaMethods,
+        filepath.Join(path, "mfa_methods"),
+        func(id string, it models.TreeMFAMethod) string { return it.Mechanism }); err != nil {
+        return err
+    }
 
-	if err = writeFiles(model.Themes,
-		filepath.Join(path, "themes"),
-		func(id string, it models.TreeTheme) string { return it.Name }); err != nil {
-		return err
-	}
+    for _, theme := range model.Themes {
+        var (
+            themePath   = filepath.Join(path, "themes", normalize(theme.Name))
+            themeConfig models.Rfc7396PatchOperation
+        )
 
-	for k, server := range model.Servers {
-		opts = append(opts, api.WithWorkspace(k))
-		var serverData models.Rfc7396PatchOperation
-		if serverData, err = utils.FromModelToPatch(&server); err != nil {
-			return err
-		}
+        if themeConfig, err = utils.FromModelToPatch(&theme); err != nil {
+            return err
+        }
 
-		if err = t.ServerStorage.Write(ctx, serverData, opts...); err != nil {
-			return err
-		}
-	}
+        delete(themeConfig, "templates")
 
-	return nil
+        if err = writeFile(themeConfig, filepath.Join(themePath, "theme")); err != nil {
+            return err
+        }
+
+        if err = storeTemplates(theme.Templates, filepath.Join(themePath, "templates")); err != nil {
+            return err
+        }
+    }
+
+    for k, server := range model.Servers {
+        opts = append(opts, api.WithWorkspace(k))
+        var serverData models.Rfc7396PatchOperation
+        if serverData, err = utils.FromModelToPatch(&server); err != nil {
+            return err
+        }
+
+        if err = t.ServerStorage.Write(ctx, serverData, opts...); err != nil {
+            return err
+        }
+    }
+
+    return nil
 }
 
 func (t *TenantStorage) Read(ctx context.Context, opts ...api.SourceOpt) (models.Rfc7396PatchOperation, error) {
-	var (
-		path       = t.Config.DirPath
-		tenant     models.Rfc7396PatchOperation
-		options    = &api.Options{}
-		workspaces []string
-		err        error
-	)
+    var (
+        path       = t.Config.DirPath
+        tenant     models.Rfc7396PatchOperation
+        options    = &api.Options{}
+        themeDirs  []string
+        workspaces []string
+        err        error
+    )
 
-	for _, opt := range opts {
-		opt(options)
-	}
+    for _, opt := range opts {
+        opt(options)
+    }
 
-	if tenant, err = readFile(filepath.Join(path, "tenant")); err != nil {
-		return nil, err
-	}
+    if tenant, err = readFile(filepath.Join(path, "tenant")); err != nil {
+        return nil, err
+    }
 
-	if err = readFilesToMap(tenant, "pools", filepath.Join(path, "pools")); err != nil {
-		return nil, err
-	}
+    if err = readFilesToMap(tenant, "pools", filepath.Join(path, "pools")); err != nil {
+        return nil, err
+    }
 
-	if err = readFilesToMap(tenant, "schemas", filepath.Join(path, "schemas")); err != nil {
-		return nil, err
-	}
+    if err = readFilesToMap(tenant, "schemas", filepath.Join(path, "schemas")); err != nil {
+        return nil, err
+    }
 
-	if err = readFilesToMap(tenant, "mfa_methods", filepath.Join(path, "mfa_methods")); err != nil {
-		return nil, err
-	}
+    if err = readFilesToMap(tenant, "mfa_methods", filepath.Join(path, "mfa_methods")); err != nil {
+        return nil, err
+    }
 
-	if err = readFilesToMap(tenant, "themes", filepath.Join(path, "themes")); err != nil {
-		return nil, err
-	}
+    if err = readFilesToMap(tenant, "themes", filepath.Join(path, "themes")); err != nil {
+        return nil, err
+    }
 
-	if workspaces, err = listDirsInPath(filepath.Join(path, "workspaces")); err != nil {
-		return nil, err
-	}
+    if themeDirs, err = listDirsInPath(filepath.Join(path, "themes")); err != nil {
+        return nil, err
+    }
 
-	if len(workspaces) > 0 {
-		var servers = map[string]any{}
+    themes := models.TreeThemes{}
 
-		for _, workspace := range workspaces {
-			var workspaceConfig models.Rfc7396PatchOperation
+    for _, dir := range themeDirs {
+        var (
+            themeConfig map[string]any
+            theme       *models.TreeTheme
+        )
 
-			opts = append(opts, api.WithWorkspace(workspace), api.WithFilters([]string{}))
+        if themeConfig, err = readFile(filepath.Join(path, "themes", dir, "theme")); err != nil {
+            return nil, err
+        }
 
-			if workspaceConfig, err = t.ServerStorage.Read(ctx, opts...); err != nil {
-				return nil, err
-			}
+        if theme, err = utils.FromPatchToModel[models.TreeTheme](themeConfig); err != nil {
+            return nil, err
+        }
 
-			id := workspaceConfig["id"].(string)
-			delete(workspaceConfig, "id")
-			delete(workspaceConfig, "tenant_id")
-			servers[id] = workspaceConfig
-		}
+        var (
+            templates       *models.TreeTemplates
+            templatesConfig map[string]any
+        )
 
-		tenant["servers"] = servers
-	}
+        if templatesConfig, err = readFiles(filepath.Join(path, "themes", normalize(theme.Name), "templates")); err != nil {
+            return nil, err
+        }
 
-	if tenant, err = utils.FilterPatch(tenant, options.Filters); err != nil {
-		return nil, err
-	}
+        if templates, err = utils.FromPatchToModel[models.TreeTemplates](templatesConfig); err != nil {
+            return nil, err
+        }
 
-	return tenant, nil
+        theme.Templates = *templates
+        themes[themeConfig["name"].(string)] = *theme
+    }
+
+    if len(themes) > 0 {
+        tenant["themes"] = themes
+    }
+
+    if workspaces, err = listDirsInPath(filepath.Join(path, "workspaces")); err != nil {
+        return nil, err
+    }
+
+    if len(workspaces) > 0 {
+        var servers = map[string]any{}
+
+        for _, workspace := range workspaces {
+            var workspaceConfig models.Rfc7396PatchOperation
+
+            opts = append(opts, api.WithWorkspace(workspace), api.WithFilters([]string{}))
+
+            if workspaceConfig, err = t.ServerStorage.Read(ctx, opts...); err != nil {
+                return nil, err
+            }
+
+            id := workspaceConfig["id"].(string)
+            delete(workspaceConfig, "id")
+            delete(workspaceConfig, "tenant_id")
+            servers[id] = workspaceConfig
+        }
+
+        tenant["servers"] = servers
+    }
+
+    if tenant, err = utils.FilterPatch(tenant, options.Filters); err != nil {
+        return nil, err
+    }
+
+    return tenant, nil
 }
 
 var _ Storage = &TenantStorage{}
+
+func storeTemplates(templates models.TreeTemplates, path string) error {
+    for id, template := range templates {
+        var (
+            sc   = NewWithID(id, template)
+            name = normalize(id)
+            raw  Writer[[]byte]
+            err  error
+        )
+
+        if raw, err = RawWriter(path); err != nil {
+            return err
+        }
+
+        if err = raw(name, []byte(sc.Other.Content)); err != nil {
+            return err
+        }
+
+        sc.Other.Content = createMultilineIncludeTemplate(name, 2)
+
+        if err = writeFile(sc, filepath.Join(path, name)); err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
